74f9f691c1b4bf7d919e66db3f2ef477a45a45d3
Victor Lazaro <lazarofilm@gmail.com>
2021-01-30 15:19:39 -0500

    Connected to MongoDB

--- src/ItemsGrid.js
+++ src/ItemsGrid.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import {
   GridList,
@@ -10,7 +11,7 @@ import {
 } from "@material-ui/core";
 // import InfoIcon from "@material-ui/icons/Info";
 import AddCircleIcon from "@material-ui/icons/AddCircle";
-import tileData from "./tileData";
+// import tileData from "./tileData";
 import ItemPrev from "./ItemPrev";
 // import image from "./pro_level.jpg";
 
@@ -52,48 +53,74 @@ const useStyles = makeStyles((theme) => ({
  *   },
  * ];
  */
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+
+  async function fetchData() {
+    const response = await fetch(url)
+      .then((item) => item.json())
+      .then((res) => setData(res.item));
+    console.log(data);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [url]);
+  return data;
+};
+
 export default function TitlebarGridList() {
+  const tileData = useFetch("http://localhost:3001/api/getItem");
   const classes = useStyles();
 
   // Open/Close the backdrop
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
   const handleClose = () => {
     setOpen(false);
   };
   const handleToggle = () => {
     setOpen(!open);
   };
-
-  return (
-    <div className={classes.root}>
-      <GridList cellHeight={180} className={classes.gridList}>
-        <GridListTile key="Subheader" cols={2} style={{ height: "auto" }}>
-          <ListSubheader component="div">
-            3D printed camera accessories for Steadicam and camera operators
-          </ListSubheader>
-        </GridListTile>
-        {tileData.map((tile) => (
-          <GridListTile key={tile.title}>
-            <img src={tile.img} alt={tile.title} />
-            <GridListTileBar
-              title={tile.title}
-              subtitle={<span>{tile.description}</span>}
-              actionIcon={
-                <IconButton
-                  aria-label={`info about ${tile.title}`}
-                  className={classes.icon}
-                  onClick={handleToggle}
-                >
-                  <AddCircleIcon />
-                </IconButton>
-              }
-            />
+  if (tileData) {
+    console.log(tileData);
+    return (
+      <div className={classes.root}>
+        <GridList cellHeight={180} className={classes.gridList}>
+          <GridListTile key="Subheader" cols={2} style={{ height: "auto" }}>
+            <ListSubheader component="div">
+              3D printed camera accessories for Steadicam and camera operators
+            </ListSubheader>
           </GridListTile>
-        ))}
-      </GridList>
-      <Backdrop className={classes.backdrop} open={open} onClick={handleClose}>
-        <ItemPrev />
-      </Backdrop>
-    </div>
-  );
+          {tileData.map((tile) => (
+            <GridListTile key={tile.title}>
+              <img src={tile.img} alt={tile.title} />
+              <GridListTileBar
+                title={tile.title}
+                subtitle={<span>{tile.description}</span>}
+                actionIcon={
+                  <IconButton
+                    aria-label={`info about ${tile.title}`}
+                    className={classes.icon}
+                    onClick={handleToggle}
+                  >
+                    <AddCircleIcon />
+                  </IconButton>
+                }
+              />
+            </GridListTile>
+          ))}
+        </GridList>
+        <Backdrop
+          className={classes.backdrop}
+          open={open}
+          onClick={handleClose}
+        >
+          <ItemPrev />
+        </Backdrop>
+      </div>
+    );
+  } else {
+    return <div>loading...</div>;
+  }
 }
