89dbc24ee978a8303d671b2226021b3dd99b0e99
Victor Lazaro <lazarofilm@gmail.com>
2021-03-06 17:05:02 -0500

    404 page tweak

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -1,9 +1,11 @@
 import React from "react";
+import lost from "../images/Lost_sign.jpg";
 
 const NotFound = () => {
   return (
     <div>
       <h1>404 Not Found</h1>
+      <img src={lost} alt="You seem lost..." />
     </div>
   );
 };
