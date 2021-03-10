4900433880cab1d6698f50befcbc3dbb29b60720
Victor Lazaro <lazarofilm@gmail.com>
2021-03-10 17:13:26 -0500

    Started over on item.js

--- frontend/src/components/Item.js
+++ frontend/src/components/Item.js
@@ -1,66 +1,131 @@
 import "./Item.css";
 
 import {
-  Button,
+  Avatar,
   Card,
-  CardActionArea,
   CardActions,
   CardContent,
+  CardHeader,
   CardMedia,
+  Collapse,
+  IconButton,
   Typography,
 } from "@material-ui/core";
+import {
+  Close as CloseIcon,
+  ExpandMore as ExpandMoreIcon,
+  Favorite as FavoriteIcon,
+  Share as ShareIcon,
+} from "@material-ui/icons";
 
+import { Link } from "react-router-dom";
 import React from "react";
+import clsx from "clsx";
 import image from "../images/pro_level.jpg";
 import { makeStyles } from "@material-ui/core/styles";
+import { red } from "@material-ui/core/colors";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
-    width: 845,
-    // display: "flex",
+    width: "60%",
   },
   media: {
-    height: 340,
+    height: 0,
+    paddingTop: "56.25%", // 16:9
   },
-});
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: "rotate(180deg)",
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
 
 export default function Item(item) {
   const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
 
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
   return (
-    // <Backdrop className={classes.backdrop} open="open">
-    <Card
-      className={classes.root}
-      onClick={console.log("You clicked on a Card")}
-    >
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image={image}
-          title="Contemplative Reptile"
-        />
+    <Card className={classes.root}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings" component={Link} to="/">
+            <CloseIcon />
+          </IconButton>
+        }
+        title="Shrimp and Chorizo Paella"
+        subheader="September 14, 2016"
+      />
+      <CardMedia className={classes.media} image={image} title="Paella dish" />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          Test
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Pro Level
+          <Typography paragraph>Method:</Typography>
+          <Typography paragraph>
+            Heat 1/2 cup of the broth in a pot until simmering, add saffron and
+            set aside for 10 minutes.
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Make balancing your camera a breeze
+          <Typography paragraph>
+            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
+            over medium-high heat. Add chicken, shrimp and chorizo, and cook,
+            stirring occasionally until lightly browned, 6 to 8 minutes.
+            Transfer shrimp to a large plate and set aside, leaving chicken and
+            chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes,
+            onion, salt and pepper, and cook, stirring often until thickened and
+            fragrant, about 10 minutes. Add saffron broth and remaining 4 1/2
+            cups chicken broth; bring to a boil.
+          </Typography>
+          <Typography paragraph>
+            Add rice and stir very gently to distribute. Top with artichokes and
+            peppers, and cook without stirring, until most of the liquid is
+            absorbed, 15 to 18 minutes. Reduce heat to medium-low, add reserved
+            shrimp and mussels, tucking them down into the rice, and cook again
+            without stirring, until mussels have opened and rice is just tender,
+            5 to 7 minutes more. (Discard any mussels that don’t open.)
+          </Typography>
+          <Typography>
+            Set aside off of the heat to let rest for 10 minutes, and then
+            serve.
           </Typography>
         </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button
-          size="small"
-          color="secondary"
-          onClick={console.log(`like ${item}`)}
-        >
-          Like
-        </Button>
-        <Button size="small" color="secondary">
-          Buy
-        </Button>
-      </CardActions>
+      </Collapse>
     </Card>
-    // </Backdrop>
   );
 }
---
+++ frontend/src/components/OLD/OLD_Item.js
@@ -0,0 +1,63 @@
+import "./Item.css";
+
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@material-ui/core";
+
+import React from "react";
+import image from "../images/pro_level.jpg";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root: {
+    width: 845,
+    // display: "flex",
+  },
+  media: {
+    height: 340,
+  },
+});
+
+export default function Item(item) {
+  const classes = useStyles();
+
+  return (
+    // <Backdrop className={classes.backdrop} open="open">
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={image}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Pro Level
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Make balancing your camera a breeze
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button
+          size="small"
+          color="secondary"
+          onClick={console.log(`like ${item}`)}
+        >
+          Like
+        </Button>
+        <Button size="small" color="secondary">
+          Buy
+        </Button>
+      </CardActions>
+    </Card>
+    // </Backdrop>
+  );
+}
